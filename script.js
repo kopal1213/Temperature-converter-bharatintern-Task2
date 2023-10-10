391b1fdd5933fbf5a41b9a2c49995a319369f516
Kopal Jain <105218964+kopal1213@users.noreply.github.com>
2023-10-10 17:12:24 +0530

    done

---
+++ script.js
@@ -0,0 +1,27 @@
+const CalculateTemp = () => {
+    const inputTemp = document.getElementById('temp').value;
+
+    const tempSelected = document.getElementById('temp_diff');
+
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+
+    //Celsius to fahrenheit
+    const celtofah = (cel) => {
+        let fahrenheit = ((cel * 9 / 5) + 32).toFixed(1);
+        return fahrenheit;
+    }
+
+    //fahrenheit to celsius
+    const fahtocel = (fah) => {
+        let celsius = ((fah - 32)* 5 / 9).toFixed(1);
+        return celsius;
+    }
+
+    if(valueTemp == 'cel'){
+        document.getElementById("result").innerHTML = celtofah(inputTemp) + "&#176; Fahrenheit";
+    }
+    else{
+        document.getElementById("result").innerHTML = fahtocel(inputTemp) + "&#176; Celsius";
+    }
+    
+}
